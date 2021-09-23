f8cde6bcfe0ffff6a4357185a7d87afffa313fd3
WiniFN <winidhila@gmail.com>
2021-09-23 19:31:38 +0700

    menambahkan Tugas 2_Biodata

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+ class Contoh1 extends CI_Controller
+ {
+	 public function index()
+	 {
+		 echo"<h1>Perkenalkan</h1><br>";
+		 echo"Nama saya Wini Fadhilah Nugraha,Saya tinggal di Jl.Ciakar Wuluh RT/002 RW/010,Kelurahan Ciakar,Kecamatan Cibeureum,Tasikmalaya.Nomor HP saya 087754686092.
+		      Olahraga yang saya sukai adalah Senam dan BuluTangkis.Hobi saya adalah membaca dan menari.";
+	 }
+ }	 	 
